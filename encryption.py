8592ec05f33d9b8c17f0d94c6c8216322af2932b
Yogesh Aggarwal <developeryogeshgit@gmail.com>
2019-12-15 09:37:06 +0530

    Converted to CLI with portable binary

--- encryption.py
+++ encryption.py
@@ -1,23 +1,28 @@
+import argparse
 import tkinter.scrolledtext as st
-from tkinter import *
-from tkinter import filedialog, messagebox
+from tkinter import Tk, Button, END, RAISED, X
 
 from cryptography.fernet import Fernet as frt
 
 font_regular="google sans regular"
 font_bold="google sans bold"
 
+encrypt_data_get = None
+decrypt_data_get = None
+
+key = b"DhQuyqPEot6MEyE3qad2iHSL5KV1V0LqPw-9ncnbxp0="
+
 def encryptor(words):
     def cipher(words):
-        result_cipher = '' #cipher text is stored in this variable
+        result_cipher = '' # cipher text is stored in this variable
         i = len(words) - 1
         while i >= 0:
             result_cipher = result_cipher + words[i]
             i = i - 1
         return result_cipher
     def fernet_en(words):
-        f=frt(b"DhQuyqPEot6MEyE3qad2iHSL5KV1V0LqPw-9ncnbxp0=")
-        result_fernet = f.encrypt(words.encode('utf-8')) #need to convert the string to bytes
+        f=frt(key)
+        result_fernet = f.encrypt(words.encode('utf-8')) # need to convert the string to bytes
         return result_fernet.decode("utf-8")
     def trans_en(words):
         decrypted = b'!@M[#Na]Bb{V\}C|;X`cZ32L1,Kd:J."H</Ge$F%^Df>S?6A54PghOi Ij&U*9Y8~T7(RklEm+WnoQ0)pqrs=tu_vwx-yz'
@@ -28,6 +33,7 @@ def encryptor(words):
     result_encryption=trans_en(cipher(fernet_en(cipher(trans_en(cipher(fernet_en(trans_en(words))))))))
     return result_encryption
 
+
 def decryptor(words):
     def cipher(words):
         result_cipher = '' #cipher text is stored in this variable
@@ -37,7 +43,7 @@ def decryptor(words):
             i = i - 1
         return result_cipher
     def fernet_de(words):
-        f=frt(b"DhQuyqPEot6MEyE3qad2iHSL5KV1V0LqPw-9ncnbxp0=")
+        f=frt(key)
         result_fernet = f.decrypt(words.encode('utf-8')) #need to convert the string to bytes
         return result_fernet.decode("utf-8")
     def trans_de(words):
@@ -49,28 +55,70 @@ def decryptor(words):
     result_encryption=trans_de(fernet_de(cipher(trans_de(cipher(fernet_de(cipher(trans_de(words))))))))
     return result_encryption
 
+
 def encrypt():
     data=encrypt_data_get.get('1.0',END+'-1c')
     encrypted_data=encryptor(data)
     decrypt_data_get.insert("1.0",encrypted_data)
+
+
 def decrypt():
     data=decrypt_data_get.get('1.0',END+'-1c')
     decrypted_data=decryptor(data)
     encrypt_data_get.insert("1.0",decrypted_data)
 
-main=Tk()
-main.title("Encrption <--> Decryption")
-main.geometry("570x500")
-main.resizable(0,0)
-main.maxsize(height=500,width=570)
-main.iconbitmap(r'Versions\encryption_icon.ico')
 
-Button(text="Encrypt",bg="white",fg="green",font=(font_bold,26),command=encrypt).pack(fill=X)
-encrypt_data_get=st.ScrolledText(main,width=50,height=8,bg="white",fg="black",font=(font_regular,13),relief=RAISED,bd=3)
-encrypt_data_get.pack()
+def graphical():
+    main=Tk()
+    main.title("Encrption <--> Decryption")
+    main.geometry("570x500")
+    main.resizable(0,0)
+    main.maxsize(height=500,width=570)
+    # main.iconbitmap(r'Versions\encryption_icon.ico')
 
-Button(text="Decrypt",bg="white",fg="red",font=(font_bold,26),command=decrypt).pack(fill=X)
-decrypt_data_get=st.ScrolledText(main,width=50,height=8,bg="white",fg="black",font=(font_regular,13),relief=RAISED,bd=3)
-decrypt_data_get.pack()
+    Button(text="Encrypt",bg="white",fg="green",font=(font_bold,26),command=encrypt).pack(fill=X)
+    encrypt_data_get=st.ScrolledText(main,width=50,height=8,bg="white",fg="black",font=(font_regular,13),relief=RAISED,bd=3)
+    encrypt_data_get.pack()
 
-main.mainloop()
+    Button(text="Decrypt",bg="white",fg="red",font=(font_bold,26),command=decrypt).pack(fill=X)
+    decrypt_data_get=st.ScrolledText(main,width=50,height=8,bg="white",fg="black",font=(font_regular,13),relief=RAISED,bd=3)
+    decrypt_data_get.pack()
+
+    main.mainloop()
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-e", "--encrypt", help="Encrypt", action="store_true")
+    parser.add_argument("-d", "--decrypt", help="Decrypt", action="store_true")
+    parser.add_argument("-g", "--graphical", help="Opens graphical window", action="store_true")
+    parser.add_argument("-k", "--key", help="Key of encryption", action="store_true")
+
+    args = parser.parse_args()
+
+    if args.graphical:
+        graphical()
+    else:
+        if args.encrypt:
+            value = input("Value: ")
+            try:
+                print(f"\nEncrypted: {encryptor(value)}")
+            except Exception:
+                print("Some error occured!")
+        
+        if args.decrypt:
+            value = input("Value: ")
+            keyInp = input("Key(?): ")
+            global key
+            key = keyInp if keyInp else key
+
+            try:
+                print(f"\nDecrypted: {decryptor(value)}")
+            except Exception:
+                print("\n=> Some error occured! It might be a key or value problem! <=")
+
+        if args.key:
+            print(f"Key: {key}")
+
+
+main()
